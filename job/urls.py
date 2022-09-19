222ffd2830f0ff78f5fd8bc9e7f997c69ef03f1e
3laa3tef <104004078+3laa3tef@users.noreply.github.com>
2022-09-19 10:28:15 +0200

    add job views & url

---
+++ job/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import path , include
+from django.contrib import admin
+from  . import views
+
+urlpatterns = [
+    path('', views.job_list),
+    path('<int:id>', views.job_detail)
+
+]
--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import path , include
+from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('jobs/', include('job.urls')),
+
 ]
